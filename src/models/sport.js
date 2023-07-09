a2b984c2c894fb22fe489d131fedc9d99e9f8ffc
marcos5163 <marcoschr56@gmail.com>
2023-07-09 22:54:59 +0530

    modify endpoint to return startTime, format, matchId

--- src/models/sport.js
+++ src/models/sport.js
@@ -1,9 +1,12 @@
 const mysql = require('../lib/mysql');
 
 const getAllSportsToursAndMatches = async () => {
-    const statement = 'select s.name as sportName, t.name as tourName, m.name as matchName ' +
-        'from matches m left join tours t on m.tourId = t.id ' +
-        'left join sports s on t.sportId = s.id';
+    const statement = `
+      SELECT s.name AS sportName, t.name AS tourName, m.name AS matchName, m.id AS matchId, m.startTime, m.format
+      FROM matches AS m
+      LEFT JOIN tours AS t ON m.tourId = t.id
+      LEFT JOIN sports AS s ON t.sportId = s.id
+    `;
     const parameters = [];
     return await mysql.query(statement, parameters);
 }
